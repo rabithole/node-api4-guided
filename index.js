2cc116e7de434d5c57207aa91794af92426d773d
Robert Kulp <pedalinon@gmail.com>
2020-03-18 19:58:35 -0700

    added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+require('dotenv').config(); // This sets the port dynamically
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+
+// CI/CD pipeline stages:
+// * development (dev)
+// * test 
+// * staging 
+// * --- manual gate (a gate blocks prgress, it's a stop) (how Kirkby's office works)
+// * production 
+
+
+///////// Also look into auto generating a .gitignore file
+
+// Setting the PORT with command line commands overrides what the env file is doing. 
+// export PORT=4004 to set port value in the command line
+// unset PORT, set PORT=4004, 
